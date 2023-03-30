d5d7140b91c796748a6e12e63c94947efa238003
valechat443 <127490359+valechat443@users.noreply.github.com>
2023-03-30 10:24:55 +0200

    altezza (#2)
    * Add files via upload
    
    aggunta altezza e suoi metodi
    
    * Add files via upload
    
    aggunta altezza e suoi metodi
    
    ---------
    
    Co-authored-by: Valentino Chatrian <ICTS22-24.062@studenti.local>
    Co-authored-by: TempoTea <127490540+TempoTea@users.noreply.github.com>

--- Persona.py
+++ Persona.py
@@ -1,8 +1,9 @@
 class Persona:
-    def __init__(self, nome:str, cognome:str, eta:int):
+    def __init__(self, nome:str, cognome:str, eta:int, altezza: float):
         self.__nome = nome
         self.__cognome = cognome
         self.__eta = eta
+        self.__altezza = altezza
         
     def getNome(self):
         return self.__nome
@@ -13,14 +14,23 @@ class Persona:
     def getEta(self):
         return self.__eta
     
-    def setNome(self,nome):
+    def getAltezza(self):
+        return self.__altezza
+    
+    def setNome(self, nome):
         self.__nome = nome
         
-    def setCognome(self,cognome):
+    def setCognome(self, cognome):
         self.__cognome = cognome
     
     def setEta(self, eta):
         self.__eta = eta
+    
+    def setAltezza(self, altezza):
+        self.__altezza = altezza
+    
+    def getAltezzaInPiedi(self):
+        return self.__altezza*0.0328084
         
     def __str__(self) :
-        return "Nome: " + self.__nome + "\nCognome: "+ self.__cognome + "\nEtà: " + str(self.__eta)
+        return "Nome: " + self.__nome + "\nCognome: "+ self.__cognome + "\nEtà: " + str(self.__eta) + "\nAltezza in cm: " + str(self.__altezza)
