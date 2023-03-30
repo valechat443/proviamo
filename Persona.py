d6eded3b8a67bd7965bf240ae956ceddc2bb7259
TempoTea <127490540+TempoTea@users.noreply.github.com>
2023-03-30 10:15:04 +0200

    Aggiunto oggetto "età" + modificato __str__ (#1)
    ripulito un po' di roba
    
    Co-authored-by: Giorgio Magliocca <ICTS22-24.454@studenti.local>

--- Persona.py
+++ Persona.py
@@ -1,16 +1,26 @@
 class Persona:
-    def __init__(self,nome,cognome) :
-        self.__nome=nome
-        self.__cognome=cognome
+    def __init__(self, nome:str, cognome:str, eta:int):
+        self.__nome = nome
+        self.__cognome = cognome
+        self.__eta = eta
         
     def getNome(self):
         return self.__nome
+    
     def getCognome(self):
         return self.__cognome
-    def setNome(self,st):
-        self.__nome=st
-    def setCognome(self,st):
-        self.__cognome=st
+    
+    def getEta(self):
+        return self.__eta
+    
+    def setNome(self,nome):
+        self.__nome = nome
+        
+    def setCognome(self,cognome):
+        self.__cognome = cognome
+    
+    def setEta(self, eta):
+        self.__eta = eta
         
     def __str__(self) :
-        return "nome: "+self.__nome+"\ncognome: "+self.__cognome
+        return "Nome: " + self.__nome + "\nCognome: "+ self.__cognome + "\nEtà: " + str(self.__eta)
