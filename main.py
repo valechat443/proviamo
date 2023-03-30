f3ab892995cc135e1494bb612e8703170d80dc90
TempoTea <127490540+TempoTea@users.noreply.github.com>
2023-03-30 10:47:58 +0200

    file main per classe Persona

---
+++ main.py
@@ -0,0 +1,11 @@
+from Persona import Persona
+
+def main():  
+    
+    p1 = Persona("Pietro", "Castagna", 22, 175)
+    p2 = Persona("Antonio", "Esposito", 46, 180)
+    p3 = Persona("Giorgio", "Magliocca", 20, 175)
+    
+    print(f"\n{p1}\n\n{p2}\n\n{p3}\n")
+    
+main()
