f2b4d2e2c5549206b4050e5884c5a5ec979ec85e
MrRoxa34 <101925708+MrRoxa34@users.noreply.github.com>
2023-03-30 11:14:08 +0200

    Branch3 (#5)
    * queste sono le mie modifice
    
    * Ora sei contento?
    
    ---------
    
    Co-authored-by: Paolo De Andreis <ICTS22-24.136@studenti.local>

--- Persona.py
+++ Persona.py
@@ -1,43 +1,46 @@
 class Persona:
-    def __init__(self, nome:str, cognome:str, eta:int, nazionalita:str, altezza: float):
-        self.__nome = nome
-        self.__cognome = cognome
+    def __init__(self, nome:str, cognome:str, eta:int, nazionalita:str, altezza: float,colorCapelli:str,colorOcchi:str) :
+        self.__nome=nome
+        self.__cognome=cognome
         self.__eta = eta
-        self.__nazionalita = nazionalita
         self.__altezza = altezza
+        self.__nazionalita = nazionalita
+        self.__coloreCapelli=colorCapelli
+        self.__coloreOcchi=colorOcchi
         
     def getNome(self):
         return self.__nome
-    
     def getCognome(self):
         return self.__cognome
-    
     def getEta(self):
         return self.__eta
-    
     def getNazionalita(self):
         return self.__nazionalita
-    
     def getAltezza(self):
         return self.__altezza
-    
-    def setNome(self, nome):
-        self.__nome = nome
-        
-    def setCognome(self, cognome):
-        self.__cognome = cognome
-    
-    def setEta(self, eta):
-        self.__eta = eta
-    
-    def setAltezza(self, altezza):
-        self.__altezza = altezza
-        
-    def setNazionalita(self, nazionalita):
-        self.__nazionalita = nazionalita
-    
+    def getColoreOcchi(self):
+        return self.__coloreOcchi
+    def getColoreCapelli(self):
+        return self.__coloreCapelli
     def getAltezzaInPiedi(self):
         return self.__altezza*0.0328084
+
+    def setNome(self, nome):
+        self.__nome = nome
+    def setCognome(self, cognome):
+        self.__cognome = cognome
+    def setEta(self, eta):
+        self.__eta = eta
+    def setAltezza(self, altezza):
+        self.__altezza = altezza
+    def setNazionalita(self, nazionalita):
+        self.__nazionalita = nazionalita
+    def setColoreCapelli(self, colCapel):
+        self.__coloreCapelli = colCapel
+    def setColoreOcchi(self, colOcchi):
+        self.__coloreOcchi = colOcchi
+
+
         
     def __str__(self) :
-        return "Nome: " + self.__nome + "\nCognome: "+ self.__cognome + "\nEtà: " + str(self.__eta) +"\nnazionalita: "+self.__nazionalita+ "\nAltezza in cm: " + str(self.__altezza)
+        return "Nome: " + self.__nome + "\nCognome: "+ self.__cognome + "\nEtà: " + str(self.__eta) +"\nnazionalita: "+self.__nazionalita+ "\nAltezza in cm: " + str(self.__altezza) + "\nColore Occhi: " + self.__coloreOcchi + "\nColore Capelli: " + self.__coloreCapelli
